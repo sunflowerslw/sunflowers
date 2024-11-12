4e9a4edcfda97368834f6ef8a2b20cf51b598d99
sunflowerslw <sunflowerslw0607@gmail.com>
2024-11-13 01:41:50 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import StudyRoom from './components/StudyRoom'; // 确保路径正确
+
+function App() {
+    return (
+        <div className="App">
+            <StudyRoom />
+        </div>
+    );
+}
+
+export default App;
